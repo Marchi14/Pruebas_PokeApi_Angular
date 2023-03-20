b59892ddf97b242b5da2e55aefe056d5ef3e9935
Marchi14 <45884562+Marchi14@users.noreply.github.com>
2023-03-21 00:37:07 +0100

    Styles

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import 'zone.js/dist/zone';
-//import { enableProdMode } from '@angular/core';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { bootstrapApplication } from '@angular/platform-browser';
-/*
-if (true) {
-  enableProdMode();
-}
-*/
+
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule, PokemonComponent],
-  template: `<div class="container"><app-pokemon></app-pokemon></div>`,
+  template: `<div class="container"><app-pokemon class="row"></app-pokemon></div>`,
 })
 export class App {
   name = 'Angular';
